8362c2b91c6afac9df72623ec10d81c3c0656186
Bianca Torres <38960971+BnkTCh@users.noreply.github.com>
2021-10-21 13:28:36 -0500

    Update sshconnect.groovy

--- vars/sshconnect.groovy
+++ vars/sshconnect.groovy
@@ -1,5 +1,5 @@
-def connect()
-  shagent(credentials : ['3dfc7ab3-ca16-4a72-acdd-234af2acc0d9']) {
+def connect(id)
+shagent(credentials : ['${id}']) {
                     sh '''
                         ssh -o StrictHostKeyChecking=no remote_user@remote_host 'bash -s << 'ENDSSH'
                         echo "hola como estas" > /tmp/archivo
