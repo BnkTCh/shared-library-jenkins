106bc1dd6f4356cc3c5fbe814e391cba869eafa3
Bianca Torres <38960971+BnkTCh@users.noreply.github.com>
2021-10-21 16:40:59 -0500

    Update sshconnect.groovy

--- vars/sshconnect.groovy
+++ vars/sshconnect.groovy
@@ -1,8 +1,8 @@
 def call() {
   command1 = "'echo \"hola como estas\" > /tmp/archivo'"
-  command2 = "'echo \"bianca\" > /tmp/biancafile'"
+  command2 = "'echo bianca > /tmp/biancafile'"
   sshagent(credentials : ['3dfc7ab3-ca16-4a72-acdd-234af2acc0d9']) {
-    def sshCmd = steps.sh script: "ssh -o StrictHostKeyChecking=no remote_user@remote_host ${command1};${command2}"
+    def sshCmd = steps.sh script: "ssh -o StrictHostKeyChecking=no remote_user@remote_host ${command1};${command2};"
     sh(sshCmd)
   }
 }
