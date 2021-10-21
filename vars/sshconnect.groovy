621ab25f1db3573a110c098bb68ed3d735cd088e
Bianca Torres <38960971+BnkTCh@users.noreply.github.com>
2021-10-21 16:05:51 -0500

    Update sshconnect.groovy

--- vars/sshconnect.groovy
+++ vars/sshconnect.groovy
@@ -4,7 +4,7 @@ def call() {
        ssh -o StrictHostKeyChecking=no remote_user@remote_host 'bash -s << 'ENDSSH'
        echo "hola como estas" > /tmp/archivo
        echo "bianca" > /tmp/biancafile
-    ENDSSH'
+  ENDSSH'
        '''.stripIndent()
   }
 }
