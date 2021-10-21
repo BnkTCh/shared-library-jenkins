17bb71c63c6bd3ce8d00586772df2bac1d70e14f
Bianca Torres <38960971+BnkTCh@users.noreply.github.com>
2021-10-21 15:36:11 -0500

    Update sshconnect.groovy

--- vars/sshconnect.groovy
+++ vars/sshconnect.groovy
@@ -1,2 +1,6 @@
-def sshconnect = sh returnStdout: true, script: "ssh -o StrictHostKeyChecking=no remote_user@remote_host 'cat /etc/os-release'"
-sh(sshconnect)
+def call() {
+  shagent(credentials : ['3dfc7ab3-ca16-4a72-acdd-234af2acc0d9']) {
+    def sshCmd = sh returnStdout: true, script: "ssh -o StrictHostKeyChecking=no remote_user@remote_host 'cat /etc/os-release'"
+    sh(sshCmd)
+  }
+}
