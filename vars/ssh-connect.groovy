6a491aaac2d5cfc7b8b634ba339c97bbb5fb1db3
Bianca Torres <38960971+BnkTCh@users.noreply.github.com>
2021-10-21 13:22:03 -0500

    Update and rename ssh-connect.groovy to sshconnect.groovy

--- vars/ssh-connect.groovy
+++
@@ -1,6 +0,0 @@
-def sshconnect(){
-  ssh -o StrictHostKeyChecking=no remote_user@remote_host uptime
-  echo "hola" > /tmp/archivo
-  scp /tmp/archivo remote_user@remote_host:/tmp/archivo
-  rm -rf /tmp/archivo
-}
---
+++ vars/sshconnect.groovy
@@ -0,0 +1,9 @@
+def connect()
+  shagent(credentials : ['3dfc7ab3-ca16-4a72-acdd-234af2acc0d9']) {
+                    sh '''
+                        ssh -o StrictHostKeyChecking=no remote_user@remote_host 'bash -s << 'ENDSSH'
+                        echo "hola como estas" > /tmp/archivo
+                        echo "bianca" > /tmp/biancafile
+                ENDSSH'
+                        '''.stripIndent()
+  }
